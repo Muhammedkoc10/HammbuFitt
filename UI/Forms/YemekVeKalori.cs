4e9aa44a48caf0686d6138cd756f5bfb84a1f921
Muhammed <muhammedkoc10@gmail.com>
2022-08-24 13:31:03 +0300

    Button Spesification changed

--- UI/Forms/YemekVeKalori.cs
+++ UI/Forms/YemekVeKalori.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HammbuuFittDal;
+using HammbuuFittData;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,7 +18,7 @@ namespace UI.Forms
         {
             InitializeComponent();
         }
-
+        Context db;
         private void YemekVeKalori_FormClosed(object sender, FormClosedEventArgs e)
         {
             Application.Exit();
@@ -27,6 +29,55 @@ namespace UI.Forms
             AnaSayfa anaSayfa = new AnaSayfa();
             this.Hide();
             anaSayfa.Show();
+        }
+
+        private void YemekVeKalori_Load(object sender, EventArgs e)
+        {
+            db = new Context();
+            List<Category> kategoriler = db.Kategoriler.ToList();
+            foreach (Category item in kategoriler)
+            {
+                cmbKaloriVeBesinKategoriSec.Items.Add(item.CategoryName);
+                cmbKategoriEkleKategoriSec.Items.Add(item.CategoryName);
+            }
+        }
+
+        private void cmbKaloriVeBesinKategoriSec_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cmbKaloriVeBesinBesinSec.Items.Clear();
+            cmbKaloriVeBesinBesinSec.Text = "";
+            for (int i = 0; i < cmbKaloriVeBesinKategoriSec.Items.Count; i++)
+            {
+                if (cmbKaloriVeBesinKategoriSec.SelectedIndex == i)
+                    CmbFill(i + 1);
+            }
+        }
+        void CmbFill(int x)
+        {
+            List<Food> foods = db.Yemekler.Where(w => w.CategoryID == x).ToList();
+            foreach (Food item in foods)
+            {
+                cmbKaloriVeBesinBesinSec.Items.Add(item.FoodName);
+            }
+        }
+
+        private void btnKaloriVeBesinGoster_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < cmbKaloriVeBesinBesinSec.Items.Count; i++)
+            {
+                if (cmbKaloriVeBesinBesinSec.SelectedIndex==i)
+                {
+                    DgvFill(i+1);
+                }
+            }
+            
+        }
+
+        private void DgvFill(int v)
+        {
+            var foodCalories = db.Yemekler.Where(w => w.FoodID == v).Select(x=>new {x.FoodName, x.Calories }).ToList();
+            dgvYemekKalori.DataSource = (foodCalories);
+
         }
     }
 }
